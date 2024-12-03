464816ff88da99ef385a868e0b3265c118098737
Atif-Rizk706 <atifrizk88@gmail.com>
2024-12-03 13:50:39 +0200

    database migration and models of tables

---
+++ app/Models/Business.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Business extends Model
+{
+    use HasFactory;
+    protected $table='business';
+    protected $fillable=[
+        'name',
+        'user_id',
+        'opening_hours',
+        'status',
+    ];
+    public function user(){
+        return $this->belongsTo(User::class);
+
+    }
+
+
+}
